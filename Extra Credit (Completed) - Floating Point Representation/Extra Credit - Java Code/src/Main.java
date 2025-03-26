58b890b6a9cc815bce48627b4268dbaae09c9f7f
NiccyNet1989 <143370847+NiccyNet1989@users.noreply.github.com>
2025-03-26 12:25:32 -0700

    Updated NFA to no longer allow negatives
    Python Floating Point Literal is actually not supposed to allow negative values, so q0 was removed
    
    Also updated q7 to allow exponents that start with a +
    So, acceptable exponents are e+, e-, e, E+, E-, e
    
    Had to update NFA on JFlap, NFA jpg image, and main file in source code.

--- Extra Credit (Completed) - Floating Point Representation/Extra Credit - Java Code/src/Main.java
+++ Extra Credit (Completed) - Floating Point Representation/Extra Credit - Java Code/src/Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Initialize states to generate completed state definitions later
-        State q0 = new State("q0"), q1 = new State("q1"), q2 = new State("q2"), q3 = new State("q3"), q4 = new State("q4");
+        State q1 = new State("q1"), q2 = new State("q2"), q3 = new State("q3"), q4 = new State("q4");
         State q5 = new State("q5"), q6 = new State("q6"), q7 = new State("q7"), q8 = new State("q8");
         State q9 = new State("q9"), q10 = new State("q10"), q11 = new State("q11");
 
         //Now add transitions to states
-        q0.addTransitions("λ-", q1);
         q1.addTransitions("0123456789", q1);
         q1.addTransitions("0123456789", q2);
         q1.addTransitions(".", q4);
@@ -20,33 +19,37 @@ public class Main {
         q5.addTransitions("_", q6);
         q5.addTransitions("eE", q7);
         q6.addTransitions("0123456789", q5);
-        q7.addTransitions("λ-", q8);
+        q7.addTransitions("+-λ", q8);
         q8.addTransitions("0123456789", q9);
         q9.addTransitions("0123456789", q9);
         q9.addTransitions("_", q10);
         q10.addTransitions("0123456789", q9);
         q11.addTransitions("0123456789", q1);
 
-        NFA decimalReaderNFA = new NFA(new State[]{q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10}, q0, new State[]{q3, q5, q9});
+        NFA decimalReaderNFA = new NFA(new State[]{q1, q2, q3, q4, q5, q6, q7, q8, q9, q10}, q1, new State[]{q3, q5, q9});
 
 //        decimalReaderNFA.printTransitions();
 //        decimalReaderNFA.printLiveStates();
 
         Scanner scanner = new Scanner(System.in);
         String input = "";
+        boolean first = true;
 
         while (true) {
-            System.out.print("Please input any string to test Floating Point Literal NFA\n*Input the string 'exit' to end the program\n\n");
-            input = scanner.nextLine();
+            if (first) {
+                System.out.print("Please input any string to test Floating Point Literal NFA\n*Input the string 'exit' to end the program\n\n");
+                input = scanner.nextLine();
+            }
 
             if (input.equals("exit")) break;
             decimalReaderNFA.isValidInput(input);
 
-            System.out.print("\n\nPress enter to continue\n");
+            System.out.print("\n\nPlease input another value to test\n*Input the string 'exit' to end the program\n\n");
             scanner = new Scanner(System.in);
-            scanner.nextLine();
+            input = scanner.nextLine();
 
             largeIndent();
+            first = false;
         }
     }
 
